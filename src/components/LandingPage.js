19ede0c6005788a04d09789af59d9353c1967cbf
AndrewKaranja <andywanji56@gmail.com>
2021-03-22 16:55:35 +0300

    added get started section with background image

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,11 +7,16 @@ import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/core/Menu'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import { PlayCircleFilledWhite } from '@material-ui/icons';
 import logo from '../qrimage.png';
+import wallpaper from '../wallpaper03.png';
+import Card from '@material-ui/core/Card' 
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles=makeStyles((theme)=>({
-root:{
+nav:{
     flexGrow:1,
 },
 menuLogo:{
@@ -23,31 +28,45 @@ title:{
 },
 authButton:{
     margin:theme.spacing(1),
-    
-
-
+    },
+intro:{
+    backgroundImage:  `url(${wallpaper})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
 },
+paperContainer: {
+    backgroundImage: `url(${wallpaper})`
+}
 }));
 
 function LandingPage(){
+    const title='Tukio';
     const classes=useStyles();
     return (
         <div className="LandingPage">
-            <div className={classes.root}>
-            <AppBar position="static" color="#fff">
+            <div className={classes.nav}>
+            <AppBar position="static" color="#000000" background="#000">
                 <Toolbar>
                         <img src={logo} alt="logo" className={classes.menuLogo}/>
                     
-                    
+                
                     <Typography variant="h6" className={classes.title}>
-                        Tukyio
+                        {title}
                     </Typography>
                     <Button variant="outlined"  color="inherit"  className={classes.authButton}>Login</Button>
                     <Button variant="contained"  color="primary"  className={classes.authButton}>Register</Button>
                 </Toolbar>
             </AppBar>
             </div>
-        </div>
+      
+           <div className="GetStarted">
+<Typography  variant="h5">Tukiyo is an app that lets you connect with art around you</Typography>
+<Button variant="contained">Get Started</Button>
+           </div>
+        
+           
+</div>
     )
 }
 
