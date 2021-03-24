65da007695456a0eecc90cbe734b4f18cebb9d6c
AndrewKaranja <andywanji56@gmail.com>
2021-03-24 10:44:04 +0300

    landing page styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import '../styles/LandingPage.css';
 import {makeStyles} from '@material-ui/core/styles'
+
+import logo from '../qrimage.png';
+import landingImg from '../monsterartist.svg'
+
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/core/Menu'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import logo from '../qrimage.png';
+
+
 import wallpaper from '../wallpaper03.png';
 import Card from '@material-ui/core/Card' 
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles=makeStyles((theme)=>({
 nav:{
@@ -37,11 +43,16 @@ intro:{
 },
 paperContainer: {
     backgroundImage: `url(${wallpaper})`
+},
+LandingImg: {
+maxWidth:60,
+maxHeight:40,
 }
 }));
 
 function LandingPage(){
     const title='Tukio';
+    const landingText='Tukiyo is an app that lets you connect with art around you';
     const classes=useStyles();
     return (
         <div className="LandingPage">
@@ -54,6 +65,7 @@ function LandingPage(){
                     <Typography variant="h6" className={classes.title}>
                         {title}
                     </Typography>
+                    
                     <Button variant="outlined"  color="inherit"  className={classes.authButton}>Login</Button>
                     <Button variant="contained"  color="primary"  className={classes.authButton}>Register</Button>
                 </Toolbar>
@@ -61,8 +73,23 @@ function LandingPage(){
             </div>
       
            <div className="GetStarted">
-<Typography  variant="h5">Tukiyo is an app that lets you connect with art around you</Typography>
-<Button variant="contained">Get Started</Button>
+           <Grid container spacing={3}>
+               <Grid item xs={12} sm={6}>
+                   <Grid sm={12}><Typography  variant="h5">Welcome to Tukio</Typography></Grid>
+                   <Grid sm={12}><Typography variant="h6" className={classes.title}>
+                        {landingText}
+                    </Typography></Grid>
+                   <Grid sm={12}><Button variant="contained">Get Started</Button></Grid>
+                  
+                  
+                    
+                  
+               </Grid>
+               <Grid item xs={12} sm={6}>
+                  <img src={landingImg} classname={classes.LandingImg} alt="monsterartist" />
+               </Grid>
+           </Grid>
+
            </div>
         
            
