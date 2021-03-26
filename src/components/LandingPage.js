097291475193298e7b3b73a3b476d8ab9087b2aa
AndrewKaranja <andywanji56@gmail.com>
2021-03-26 12:37:29 +0300

    more landing page improvements

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,13 +3,16 @@ import '../styles/LandingPage.css';
 import {makeStyles} from '@material-ui/core/styles'
 
 import logo from '../qrimage.png';
-import landingImg from '../monsterartist.svg'
+import ankara from '../ankarapattern.png';
+import landingImg from '../musicbird.svg'
 
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/core/Menu'
 import Typography from '@material-ui/core/Typography'
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import Button from '@material-ui/core/Button'
 
 
@@ -21,9 +24,27 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
+const navtheme= createMuiTheme({
+    typography: {
+     "fontFamily": `"Roboto", "Helvetica", "Arial","Montserrat" ,sans-serif`,
+     "fontSize": 14,
+     "fontWeightBold":600,
+     "fontWeightLight": 300,
+     "fontWeightRegular": 400,
+     "fontWeightMedium": 500
+    }
+ });
+
+ const handleClick=()=>{
+     console.log('Login')
+ }
+
 const useStyles=makeStyles((theme)=>({
 nav:{
     flexGrow:1,
+    backgroundImage:  `url(${ankara})`,
+    color:'#fff',
+    padding:theme.spacing(1),
 },
 menuLogo:{
     maxWidth: 40,
@@ -34,6 +55,8 @@ title:{
 },
 authButton:{
     margin:theme.spacing(1),
+    fontStyle:'bold',
+    fontSize:18,
     },
 intro:{
     backgroundImage:  `url(${wallpaper})`,
@@ -49,7 +72,9 @@ height:30,
 width:30,
 },
 description:{
-    color:"#ffffff"
+    color:"#ffffff",
+    marginBottom:10,
+    fontFamily: 'Kiwi Maru',
 }
 }));
 
@@ -59,38 +84,41 @@ function LandingPage(){
     const classes=useStyles();
     return (
         <div className="LandingPage">
-            <div className={classes.nav}>
-            <AppBar position="static" color="#000000" background="#000">
+        <ThemeProvider theme={navtheme}>
+            <AppBar position="static" color="#000000" className={classes.nav}>
                 <Toolbar>
-                        <img src={logo} alt="logo" className={classes.menuLogo}/>
-                    
-                
+                    <img src={logo} alt="logo" className={classes.menuLogo}/>
                     <Typography variant="h6" className={classes.title}>
                         {title}
                     </Typography>
-                    
                     <Button variant="outlined"  color="inherit"  className={classes.authButton}>Login</Button>
                     <Button variant="contained"  color="primary"  className={classes.authButton}>Register</Button>
                 </Toolbar>
             </AppBar>
-            </div>
+        </ThemeProvider>
+            
       
-           <div className="GetStarted">
-           <Grid container spacing={3}>
-               <Grid item xs={12} sm={6}>
-                   <Grid sm={12}><Typography  variant="h4"  className={classes.description}>Welcome to Tukio</Typography></Grid>
-                   <Grid sm={12} ><Typography variant="h6">
+        <Grid container className="GetStarted" height='100%'> 
+                <Grid item xs={12} sm={6}>
+                   <Grid sm={12}>
+                       <Typography  variant="h4"  className={classes.description} >
+                           Welcome to Tukio
+                        </Typography>
+                    </Grid>
+                    <Grid sm={12} >
+                       <Typography variant="h6" style={{marginBottom:10}}>
                         {landingText}
-                    </Typography></Grid>
-                   <Grid sm={12}><Button variant="outlined">Get Started</Button></Grid>
-                  
+                       </Typography>
+                    </Grid>
+                    <Grid sm={12}><Button variant="outlined" style={{fontStyle:'bold'}}>Get Started</Button></Grid>
                 </Grid>
-               <Grid container xs={12} sm={6} alignItems="center" justify="center" >
+
+                <Grid container xs={12} sm={6} alignItems="center" justify="center" >
                   <img src={landingImg} classname={classes.LandingImg} alt="monsterartist" />
                </Grid>
-           </Grid>
+        </Grid>
 
-           </div>
+          
         
            
 </div>
