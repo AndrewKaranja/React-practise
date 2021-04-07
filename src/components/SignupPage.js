94c261cb6a197877273754cb8947778702c1630c
AndrewKaranja <andywanji56@gmail.com>
2021-04-07 12:17:50 +0300

    update to sign up page

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button'
 import { Typography } from '@material-ui/core';
 import TextField from './TextField'
 import signupAnim from '../images/usersignup.gif'
@@ -14,18 +15,28 @@ import signupAnim from '../images/usersignup.gif'
 import {useField,Formik,Form} from 'formik'
 
 
+
 const useStyles=makeStyles((theme)=>({
     Root:{
-        backgroundColor:"#f4a261",
+        backgroundColor:"#fce3c5",
         height:'100vh',
+        width:'100vw',
         padding:theme.spacing(5),
     },
     MainGrid:{
         borderRadius:'20px',
         backgroundColor:'#fff',
         minHeight:'80vh',
-        margin:theme.spacing(3),
-        padding:theme.spacing(3),
+        minWidth:'70vw',
+        maxWidth:'70vw',
+        position:'absolute',
+        left:'50%',
+        top:'50%',
+        transform:'translate(-50%,-50%)',
+        padding:theme.spacing(4),
+    },
+    SignFormGrid:{
+        padding:theme.spacing(1),
     }
     }));
 
@@ -37,7 +48,7 @@ const SignupPage = () => {
         <div>
             <Paper className={classes.Root}>
               <Grid className={classes.MainGrid}  container direction="row">
-                  <Grid item xs={12} sm={6}>
+                  <Grid item xs={12} sm={6} className={classes.SignFormGrid}>
                   
                       <Formik
                       initialValues={{
@@ -62,14 +73,16 @@ const SignupPage = () => {
 
                                   </Form>
 
+                                  
+
                               </div>
                           )}
-
-                      </Formik>
+                          </Formik>
+                          <Button variant="contained"  color="primary"  className={classes.authButton}>Create Account</Button>
                   </Grid>
                   <Grid item xs={12} sm={6} justify="center"
-  alignItems="center">
-                 <img src={signupAnim} height="400px" width="400px" alt="signup" />
+  className="gridImgSignup" >
+                 <img src={signupAnim} height="400px" width="400px" alt="signup" className="imgSignup"/>
 
                   </Grid>
                  
