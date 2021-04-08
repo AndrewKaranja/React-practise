b75557fd3b10fde290c374397c74c61537c8f2fd
AndrewKaranja <andywanji56@gmail.com>
2021-04-09 00:48:00 +0300

    updated textfield class names

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {useField} from 'formik'
-import '../styles/TextField.css';
+import '../styles/SignupPage.css';
 import { StarHalfTwoTone } from '@material-ui/icons';
 
 
 function TextField({label,...props}) {
     const[field,meta]=useField(props);
     return (
-        <div style={{marginBottom:10}}>
-            <label htmlFor={field.name} className="form__label" style={{textAlign:'start',margin:'0',fontSize:'12px'}}>{label}</label>
+        <div className="form__div">
             <input className="form__input" autoComplete="off"></input>
+            <label htmlFor={field.name} className="form__label" >{label}</label>
+            
         </div>
     )
 }
