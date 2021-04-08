3845a25ab10901e3e317266849f8cae9232d2d06
AndrewKaranja <andywanji56@gmail.com>
2021-04-08 16:11:45 +0300

    updated the forms textfields ui

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import {useField} from 'formik'
+import '../styles/TextField.css';
 import { StarHalfTwoTone } from '@material-ui/icons';
 
 
 function TextField({label,...props}) {
     const[field,meta]=useField(props);
     return (
-        <div style={{marginBottom:20}}>
-            <label htmlFor={field.name} style={{textAlign:'start'}}>{label}</label>
-            <input className="form-control shadow-none" autoComplete="off"></input>
+        <div style={{marginBottom:10}}>
+            <label htmlFor={field.name} className="form__label" style={{textAlign:'start',margin:'0',fontSize:'12px'}}>{label}</label>
+            <input className="form__input" autoComplete="off"></input>
         </div>
     )
 }
