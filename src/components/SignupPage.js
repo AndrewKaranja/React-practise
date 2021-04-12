aca332b27d60982bb6db467d43dbed9954fc85d1
AndrewKaranja <andywanji56@gmail.com>
2021-04-12 16:14:08 +0300

    Added a terms and conditions checkbox

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import signupAnim from '../images/usersignup.gif';
+
 import '../styles/SignupPage.css';
 import {makeStyles} from '@material-ui/core/styles'
-
 import Divider from '@material-ui/core/Divider';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -11,9 +12,10 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import TextField from './TextField'
-import signupAnim from '../images/usersignup.gif'
 
-import {useField,Formik,Form} from 'formik'
+import {ErrorMessage,useField,Formik,Form} from 'formik';
+import * as Yup from 'yup';
+
 
 
 
@@ -41,110 +43,125 @@ const useStyles=makeStyles((theme)=>({
     }
     }));
 
-
-    const label = (
-        <Typography variant="caption">By Creating an account, you agree to our <Link href="#">
-      Privacy Policy
-  </Link>,<Link href="#">
-      Terms and Conditions
-  </Link> and <Link href="#">
-  Notifications Settings
-  </Link></Typography>
+const label = (
+        <Typography variant="caption">By Creating an account, you agree to our
+                   <Link href="#">Privacy Policy</Link>,<Link href="#">
+                    Terms and Conditions</Link> and <Link href="#">Notifications Settings</Link>
+        </Typography>
       )
 
+const CustomCheckbox=({children,...props})=> {
+        const[field,meta]=useField(props,'checkbox');
+        return (
+            <>
+                 <label className="checkbox" >
+                <input type="checkbox" {...field} {...props}></input>
+                {children}
+                </label>
+                {meta.touched && meta.error && 'is-invalid' ?(
+                  <div className="error">{meta.error}</div>
+                ):null} 
+                
+            </>
+        )
+    }
 
 
 const SignupPage = () => {
     const classes=useStyles();
-    const [state, setState] = React.useState({
-        checkedA: false,
-      });
-    
-      const handleChange = (event) => {
-        setState({ ...state, [event.target.name]: event.target.checked });
-      };
+
+
+      const validate=Yup.object({
+        fullname:Yup.string()
+        .min(3,'Must be atleas 3 characters')
+        .max(20,'Must be 20 characters or less')
+        .required('Name is required'),
+        username:Yup.string()
+        .min(3,'Must be atleas 3 characters')
+        .max(10,'Must be 10 characters or less')
+        .required('Username is required'),
+        email:Yup.string()
+        .email('Email is invalid')
+        .required('Email is required'),
+        password:Yup.string()
+        .min(6,'Password must be at least 6 characters')
+        .required('Password is required'),
+        confirmPassword:Yup.string()
+        .oneOf([Yup.ref('password'),null],'Password must match')
+        .required('Required'),
+        acceptedTerms:Yup.bool()
+        .oneOf([true],'You must accept the terms and conditons')
+        .required('Required'),
+        
+      })
+
     return (
         <div>
             <Paper className={classes.Root}>
               <Grid className={classes.MainGrid}  container direction="row">
                   <Grid item xs={12} sm={6} className={classes.SignFormGrid}>
-                  <h2>Get Started</h2>
-                  <Typography variant="caption">Already have an account? <Link href="#">
-      Login
-  </Link></Typography>
+                       <h2>Get Started</h2>
+                       <Typography variant="caption">Already have an account? <Link href="#">Login</Link>
+                       </Typography>
   
-                  <div id="gSignInWrapper">
-    <span className="label">Sign up with:</span>
-    <div id="customBtn" className="customGPlusSignIn">
-      <span className="icon">
+                        <div id="gSignInWrapper">
+                           <span className="label">Sign up with:</span>
+                           <div id="customBtn" className="customGPlusSignIn">
+                             <span className="icon">
       
-      </span>
-      <span className="buttonText">Google</span>
-    </div>
-  </div>
-<br/> 
+                             </span>
+                             <span className="buttonText">Google</span>
+                           </div>
+                        </div>
+                    <br/> 
+                    <Divider variant="middle" />
+                    <br/> 
 
-<Divider variant="middle" />
-<br/> 
-                 
-                      <Formik
+                    <Formik
                       initialValues={{
                           fullname:'',
                           username:'',
                           email:'',
                           password:'',
-                          confirmPassword:''
+                          confirmPassword:'',
+                          acceptedTerms:false,
+                          }}
+                          validationSchema={validate}
+                          onSubmit={(values,{setSubmitting,resetForm})=>{
+                              setTimeout(()=>{
+                                  alert(JSON.stringify(values,null,2));
+                                  resetForm();
+                                  setSubmitting(false);
+                              },3000)
+                          }}
+                          >
 
-                      }}>
                           {formik=>(
                               <div>
-                                  
                                   <Form>
                                       <TextField label="Full Name" name="fullname" type="text"/>
                                       <TextField label="Username" name="username" type="text"/>
                                       <TextField label="Email" name="email" type="text"/>
-                                      <TextField label="Password" name="passw" type="password"/>
-                                      <TextField label="Confirm Password" name="confirmpassw" type="password"/>
-
+                                      <TextField label="Password" name="password" type="password"/>
+                                      <TextField label="Confirm Password" name="confirmPassword" type="password"/>
+                                     
+                                      <CustomCheckbox name="acceptedTerms">
+                                          {label}
+                                      </CustomCheckbox>
+                                      <Button variant="contained"  color="primary"  className={classes.SignupButton} type="submit">{formik.isSubmitting ?'Loading...' : 'Create Account'}</Button>
                                   </Form>
-
-                                  
-
-                              </div>
+                                </div>
                           )}
-                          </Formik>
-
-    
-         
-                  </Grid>
-                  <Grid item xs={12} sm={6} justify="center"
-  className="gridImgSignup" >
-                 <img src={signupAnim} height="400px" width="400px" alt="signup" className="imgSignup"/>
-
+                      </Formik>
                   </Grid>
 
-                  <Grid item xs={12} sm={12}>
-                  <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.checkedA}
-            onChange={handleChange}
-            name="checkedA"
-            color="primary"
-          />
-        }
-        label={label}
-      />
+                  <Grid item xs={12} sm={6} justify="center" className="gridImgSignup">
+                       <img src={signupAnim} height="400px" width="400px" alt="signup" className="imgSignup"/>
                   </Grid>
-                  <Grid item xs={12} sm={12}>
-                  <Button variant="contained"  color="primary"  className={classes.SignupButton}>Create Account</Button>
-                  </Grid>
-                 
-                 
-  
 
-              </Grid>
 
+                  
+              </Grid>   
             </Paper>
             </div>
     )
